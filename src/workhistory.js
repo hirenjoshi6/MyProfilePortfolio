0aa94ad26578db3313f72d16c54b874422cfb584
Hiren Joshi <hirenjoshi0007@gmail.com>
2025-01-26 02:53:42 +0530

    Updated assets folder, css file, app load, styles and added work history file

---
+++ src/workhistory.js
@@ -0,0 +1,105 @@
+/* Change this file to get your personal Porfolio */
+  // Experience Page
+  const experience = {
+    title: "Fullstack Software Developer",
+    description: '<p align="justify" className="home-about-body">Highly motivated <i><b className="purple"> Technical Analyst and Mobile Lead with exceptional organizational skills,attention to detail, and a strong work ethic. </b></i> Passionate about continuous self-improvement in the IT industry, with expertise in Mobile development od REST API and GraphQL. Proactive, adaptable, and skilled in problem solving, time management, and fostering team collaboration. Completed a <i><b className="purple">Post Graduate Program in Artificial Intelligence and Machine Learning, increase confidence in Data Science and AI.</b> </i> Seeking a challenging role in <i><b className="purple">Data Science, machine Learning or related fields</b> </i>to leverage technical expertise and contribute to team success and progressive career advancement.</p>',
+    experiences: [
+          {
+            title: "Principle Mobile Engineer",
+            company: "Itcom Innovation Gaming Group",
+            company_url: "",
+            logo_path: "legato_logo.png",
+            duration: "Jan 2023 To Jan 2025",
+            isPresent: false,
+            location: "Metro Manila, Philippines",
+            description: [
+                "Led a team of 20 engineers in the life-cycle development of mobile games for Android and iOS platforms.",
+                "Standardized code using Agile methods, applied best practices, and introduced code analysis, speeding up project delivery and reducing defect rates.",
+                "Managed the integration of third party SDKs, ensured compliance with platform guidelines and maintained high application performance.",
+                "Managed all aspects of the development life-cycle, including requirements gathering, architecture design, coding, testing, and implementation."
+            ],
+          },
+          {
+            title: "Mobile Team Lead",
+            company: "My-Onsite Healthcare India Pvt. Ltd.",
+            company_url: "https://myonsitehealthcare.com/",
+            logo_path: "muffito_logo.png",
+            duration: "March 2020 To Jan 2023",
+            isPresent: false,
+            location: "Vadodara, Gujarat, India And Sarasota, Florida",
+            description: [
+                "Parent-Child Details: I Was working as Mobile Team Lead at Indralok Technology Pvt. Ltd. during March 2020 till Feb 2022.",
+                "Guided a team of 15 to developed and deployed scalable mobile solutions in native iOS, Android, React Native, and Flutter environments, which touched 100+ users",
+                "Implemented a new system, ensured full guidance to all team members, improved product quality and reduced time to market.",
+                "Developed robust API integration mechanisms for simple data exchange between mobile applications and backend systems.",
+                "Using an agile scrum methodology improved the speed of the development team and reduced technical costs."
+            ],
+          },
+          {
+            title: "Mobile Team Lead",
+            company: "Vetron IT & Consultancy Services",
+            company_url: "https://vetron.in/",
+            logo_path: "legato_logo.png",
+            isPresent: false,
+            duration: "May 2018 To March 2020",
+            location: "Surat, Gujarat, India",
+            description: [
+                "Directed a team of 24 to develop and deploy enterprise mobile applications, driving customer satisfaction and loyalty.",
+                "Conducted workshops on increasing team skills, assigned more challenging tasks, and mentored junior members, increasing team cohesion and project delivery.",
+                "Developed detailed documentation regarding project requirements, construction and planning.",
+                "Collaborated with product owners and stakeholders to determine project scope and deliverables."
+            ],
+          },
+          {
+            title: "Senior iOS Developer",
+            company: "Rawalwasia Group & Textlise",
+            company_url: "https://rawalwasia.in/",
+            logo_path: "freecopy_logo.png",
+            isPresent: false,
+            duration: "Jan 2017 To April 2018",
+            location: "Surat, Gujarat, India",
+            description: [
+                "Developed and maintained native iOS applications to ensure high performance and user satisfaction.",
+                "Swift, Objective-C, and SwiftUI were used to create an efficient and effective solution.",
+                "APIs were integrated to communicate with back-end services and data platforms.", 
+                "Provided technical leadership on projects and mentored juniors."
+            ],
+            color: "#fc1f20",
+          },
+          {
+            title: "Senior iOS Developer",
+            company: "Onus Info Tech",
+            company_url: "https://onusinfotech.com/",
+            logo_path: "muffito_logo.png",
+            duration: "September 2014 To Jan 2017",
+            isPresent: false,
+            location: "Surat, Gujarat, India",
+            description: [
+                "Designed and enhanced user interfaces for mobile apps using design patterns to enhance user experience.",
+                "Reviewed code and optimized application performance, resulting in greater user retention.",
+                "Identify opportunities to improve process through the use of automation tools and techniques.",
+                "Developed user interface modules and worked with UI/UX designers."
+            ],
+          },
+          {
+            title: "Software Engineer",
+            company: "Devlon/Grip Info Tech",
+            company_url: "https://about.me/devlon.infotech",
+            logo_path: "muffito_logo.png",
+            isPresent: false,
+            duration: "Nov 2011 To Sep 2014",
+            location: "Remote Virtual Place work",
+            description: [
+                "Android and PHP applications were developed, complementing the company’s technology portfolio.",
+                "Assisted in product development, development, implementation, and testing activities.",
+                "Worked with multidisciplinary teams to deliver high quality solutions.",
+                "Codes and recycling were developed while adhering to regulatory standards."
+            ],
+          },
+    ],
+  };
+  
+  export {
+    experience,
+  };
+  
